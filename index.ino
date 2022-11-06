cd071eebc0bc1e43bed7239ac581414b036fa4cc
MUHAMMAD RAKHA EVANDY CHRISNA <61354650+rakhaevandy27@users.noreply.github.com>
2022-11-06 07:03:08 +0700

    Update and rename NODE MCU LM35+LCD I2C.ino to index.ino

--- index.ino
+++ index.ino
@@ -1,10 +1,15 @@
+//LIBRARY
 #include <LiquidCrystal_I2C.h>
-// Konstruk object LCD dengan alamat I2C
-// Ganti 0x3F sesuai dengan alamat I2C modul kalian
-// Jika tidak tahu dapat menggunakan LCD I2C Scanner
+
+// Ganti 0x3F sesuai dengan alamat I2C modul kaliaN
+//pin scl -> d2
+//pin sda -> d3
+//pin vcc -> vcc/5v/3v (recommend 3.3v)
+//pin gnd -> gnd
 LiquidCrystal_I2C lcd(0x27, 16, 2);
  
 void setup() {
+  //SERIAL AND LCD INIT
   lcd.init();
   Serial.begin(9600); 
   lcd.begin(16,2);
@@ -15,21 +20,25 @@ void loop()
 {
   float vref = 3.3;
   float resolution = vref/1023.0;
-  float temperature = analogRead(A0);
-
+  float temperature = analogRead(A0);//pin A0 sebagai input
+  
+ //KALKULASI SUHU
   temperature = (temperature*resolution);
   temperature = temperature*100;
-
+  
+ //SERIAL OUT
   Serial.print("suhu :");
   Serial.println(temperature);
-
+  
+  //LCD OUT
   lcd.setCursor(2, 0);
   lcd.print("TEMPERATURE");
   lcd.setCursor(4, 1);
   lcd.print(temperature);
   lcd.print(" C");
   delay(3000);
-
+ 
+  //LOGIKA SUHU
   if (temperature >= 60)
   {
     lcd.clear();
